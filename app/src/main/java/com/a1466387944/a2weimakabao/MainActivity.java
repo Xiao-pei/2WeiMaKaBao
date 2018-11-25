16455e5271580fd7d313958503ff9ada91be1d3e
Xiao-pei <CIA_Xiaopei@outlook.com>
2018-11-25 11:36:05 +0800

    Init

---
+++ app/src/main/java/com/a1466387944/a2weimakabao/MainActivity.java
@@ -0,0 +1,13 @@
+package com.a1466387944.a2weimakabao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
